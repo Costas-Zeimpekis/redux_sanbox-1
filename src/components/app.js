a3c1f3e3eb21f7c90ee9f97502f444e0072bff1d
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-22 18:10:36 +0300

    Added action creators

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import Booklist from '../constainers/book-list';
+import BookDetail from '../constainers/book-detail';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Booklist />
+        <BookDetail />
       </div>
     )
   }
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// sate argument is not application state only the state the reducer is responisible for
+export default function (state = null, action) {
+
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  return state
+}
